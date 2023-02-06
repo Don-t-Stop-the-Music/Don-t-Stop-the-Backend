b252048dfea0bec9ead2ab85a5e75701ec6fa878
srw73 <sam.r.williams13@gmail.com>
2023-02-06 14:35:39 +0000

    actually analyse get frequencies

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 sample_rate = 44100
 lowest_hz = 20
 device = 1
+total_frequencies = 40
--- freq_analyser.py
+++ freq_analyser.py
@@ -1,13 +1,15 @@
 from multiprocessing import Process, Queue
 from queue import Empty
 import numpy as np
-from config import sample_rate, lowest_hz
+from config import sample_rate, lowest_hz, total_frequencies
 import math
 
-def freq_analyser_proc(audio_input):
-    max_size = math.ceil(sample_rate/lowest_hz)
+def freq_analyser_proc(audio_input, high_bandwidth_output, low_bandwidth_output):
+    analysis_size = math.ceil(sample_rate/lowest_hz) * 2
+    max_size = analysis_size * total_frequencies
     current_size = 0
     audio_block = np.full(max_size, np.nan)
+    current_analysed = 0
 
     sample_chunk, channel = audio_input.get()
     print("here")
@@ -18,8 +20,10 @@ def freq_analyser_proc(audio_input):
             #Checks if buffer already full
             if current_size + sample_chunk.size > max_size:
                 print(-(sample_chunk.size - (max_size - current_size)))
-                np.roll(audio_block, -(sample_chunk.size - (max_size - current_size)))
+                rollback = -(sample_chunk.size - (max_size - current_size))
+                np.roll(audio_block, rollback)
                 current_size = max_size - sample_chunk.size
+                current_analysed -= rollback
             # Copies new sample data into audio block
             audio_block[current_size:current_size+sample_chunk.size] = sample_chunk
             current_size = current_size + sample_chunk.size
@@ -27,7 +31,17 @@ def freq_analyser_proc(audio_input):
 
         except Empty:
             print(f"end {audio_block[-1]}")
-
+            #frequency analysis
+            working = current_analysed
+            magnitude = None
+            while ((current_size - working) >= analysis_size):
+                sample_set = audio_block[working:working + analysis_size]
+                magnitude = np.abs(np.fft.rfft(sample_set))
+                high_bandwidth_output.put((magnitude, channel))
+            #after
+            if any(magnitude):
+                low_bandwidth_output.put((magnitude, "frequency", channel))
+            current_analysed = working
             sample_chunk, channel = audio_input.get()
 
 def freq_analysers():
