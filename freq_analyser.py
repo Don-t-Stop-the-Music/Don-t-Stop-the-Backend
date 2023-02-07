225612abf333165f6af35370f5b879c6fd9133ae
srw73 <sam.r.williams13@gmail.com>
2023-02-07 10:46:46 +0000

    clean up debug code

--- freq_analyser.py
+++ freq_analyser.py
@@ -5,6 +5,7 @@ from config import sample_rate, lowest_hz, frequency_overlap
 import math
 
 def freq_analyser_proc(audio_input, high_bandwidth_output, low_bandwidth_output):
+    #magic numbers
     analysis_size = math.ceil(sample_rate/lowest_hz) * 2
     max_size = analysis_size * 40
     current_size = 0
@@ -19,20 +20,19 @@ def freq_analyser_proc(audio_input, high_bandwidth_output, low_bandwidth_output)
 
             #Checks if buffer already full
             if current_size + sample_chunk.size > max_size:
-                #print(-(sample_chunk.size - (max_size - current_size)))
                 np.roll(audio_block, -current_analysed)
                 current_size -= current_analysed
                 current_analysed = 0
+
             # Copies new sample data into audio block
             audio_block[current_size:current_size+sample_chunk.size] = sample_chunk
             current_size = current_size + sample_chunk.size
-            #print(f"audio_block end {audio_block[current_size-1]} current_size {current_size} sample_chunk {sample_chunk}")
 
         except Empty:
-            #print(f"end {audio_block[-1]}")
             #frequency analysis
             working = current_analysed
             magnitude = np.empty(int(analysis_size / 2))
+
             #analyse as many times as queue allows(is actually only once when on pc)
             while ((current_size - working) >= analysis_size):
                 sample_set = audio_block[working:working + analysis_size]
@@ -40,10 +40,10 @@ def freq_analyser_proc(audio_input, high_bandwidth_output, low_bandwidth_output)
                 for out in high_bandwidth_output:
                     out.put((magnitude, channel))
                 working += int(analysis_size / frequency_overlap) 
+
             #after
             if magnitude.any():
                 low_bandwidth_output.put((magnitude, "frequency", channel))
-                #print(f"queue updated, working: {working}")
             current_analysed = working
             sample_chunk, channel = audio_input.get()
 
