ea06821fa2c68cf2390db0e8be333705752a9d8a
BOT-Jude <96182758+BOT-Jude@users.noreply.github.com>
2023-03-07 13:19:11 +0000

    finsihed testing code and added comments

--- bluetooth_process.py
+++ bluetooth_process.py
@@ -2,6 +2,7 @@
 Bluetooth process
 sets up bluetooth connection and sends data asyncronously
 """
+import sys
 import json
 from time import sleep
 import subprocess
@@ -12,7 +13,10 @@ import bluetooth
 
 
 INITIAL_CACHE = {"frequency": [[59,75,63], [27,89,32]], "minFrequency": 0, "maxFrequency": 80,
-                  "hiss": [False, False], "feedback": [[], []]}
+                  "hiss": [False, False], "feedback": [[], []]} #
+#All data fields are named before the cache is serialized and sent for the first time
+#This prevents the client from attempting to parse fields that do not exist
+#Although the data may be bogus
 
 
 def open_server():
@@ -21,15 +25,14 @@ def open_server():
     Listen on port 1 and advertise bluetooth service
     """
 
-    discoverable()
-
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
     server_sock.bind(("", bluetooth.PORT_ANY))
     server_sock.listen(1)
     print("listening on port:", bluetooth.PORT_ANY)
 
-    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+    uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848" 
+    #uuid common between client and server, used by the client to find the server easily
 
     bluetooth.advertise_service(
         server_sock,
@@ -37,8 +40,7 @@ def open_server():
         service_id=uuid,
         service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
         profiles=[bluetooth.SERIAL_PORT_PROFILE],
-        # protocols=[bluetooth.OBEX_UUID]
-    )
+    ) #set up attributes of the bluetooth service 
 
     return server_sock
 
@@ -49,8 +51,9 @@ def connect(server_sock):
     """
 
     discoverable()
+    #bluetooth device must be 'discoverable' for the client to find the server
 
-    client_sock, address = server_sock.accept()
+    client_sock, address = server_sock.accept() #blocks until a connection is accepted
     print("Accepted connection from ", address)
 
     return client_sock
@@ -63,15 +66,19 @@ def transmit(data_stream, client_sock):
     """
 
     cache = INITIAL_CACHE
+    #initilize cache so we don't ever send an unititilized cache
 
     while True:
-        item = data_stream.get()
+        item = data_stream.get() #blocks until some/any items are available
         try:
             while True:
                 cache[item[0]] = item[1]
                 item = data_stream.get_nowait()
+                #iterates through all currently available items updating the cache
         except Empty:
+            #when out of currently available items
             print("socket response:", client_sock.send("\0" + json.dumps(cache) + "\0"))
+            #serialize cache and send to client
 
 def disconnect(client_sock, server_sock):
     """
@@ -82,7 +89,7 @@ def disconnect(client_sock, server_sock):
     client_sock.close()
     server_sock.close()
 
-    print("Closed")
+    print("Connection closed")
 
 def discoverable():
     """
@@ -91,6 +98,7 @@ def discoverable():
     """
     sleep(0.1)
     subprocess.call(['sudo', 'hciconfig', 'hci0', 'piscan'])
+    #run the console command to make pi discoverable for 60 seconds
     sleep(0.1)
 
 def bluetooth_proc(data_stream: Queue):
@@ -100,17 +108,20 @@ def bluetooth_proc(data_stream: Queue):
     """
 
     server_sock = open_server()
+    #open and set up the server
 
     while True:
 
         client_sock = connect(server_sock)
+        #wait for a connection
 
         try:
 
             transmit(data_stream, client_sock)
-            disconnect(client_sock, server_sock)
+            #transmit data
 
         except bluetooth.btcommon.BluetoothError as bluetooth_error:
+            #catch "connection reset" and "endpoint not connected" and restart connection
 
             if bluetooth_error.errno == 104:
                 print("Connection reset by peer")
@@ -118,3 +129,8 @@ def bluetooth_proc(data_stream: Queue):
                 print("Transport endpoint is not connected")
             else:
                 raise bluetooth_error
+            
+        except KeyboardInterrupt:
+            #catch keyboard interrupt and close connection (and close program)
+            disconnect(client_sock, server_sock)
+            sys.exit(0)
--- bluetooth_process_test.py
+++ bluetooth_process_test.py
@@ -13,11 +13,15 @@ def data_simulator(data_stream):
     puts sample data into the data stream to be sent over bluetooth
     """
 
-    # still need to simulate bluetooth data
-
     while True:
+
         sleep(1)
-        data_stream.put(("frequency", 0))
+        data_stream.put(("frequency", [10,20,30,40,50,60,70,80]))
+        data_stream.put(("hiss", [False,False]))
+
+        sleep(1)
+        data_stream.put(("frequency", [80,70,60,50,40,30,20,10]))
+        data_stream.put(("hiss", [True,True]))
 
 
 if __name__ == "__main__":
