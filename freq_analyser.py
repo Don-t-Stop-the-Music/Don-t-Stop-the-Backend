c877fb851afe7414fdc59dd3b55f6eed08300e1e
srw73 <sam.r.williams13@gmail.com>
2023-03-07 16:49:45 +0000

    comments for sam code and main

--- freq_analyser.py
+++ freq_analyser.py
@@ -11,6 +11,7 @@ from config import DEVICE, BLUETOOTH_SAMPLES, SAMPLE_RATE, LOWEST_FREQUENCY, FRE
 
 ANALYSIS_SIZE = math.ceil(SAMPLE_RATE / LOWEST_FREQUENCY) * 2
 
+
 def freq_analyser_proc(high_bandwidth_output, low_bandwidth_output):
     '''The frequency analyser process. 
     Process sound in and fourier transform, downsample for other processes
@@ -18,12 +19,13 @@ def freq_analyser_proc(high_bandwidth_output, low_bandwidth_output):
             high_bandwidth_output (Queue[]): A list of queues to place the full fourier array
             low_bandwidth_output (Queue): A queue to place the downsampled fourier array
     '''
-
+    # this queue buffers the raw audio data for use in analysis.
     audio_input = Queue()
-
-    print("freq_analyser online")
+    # set up the inputstream. give it callback function to put data into queue
     with sd.InputStream(device=DEVICE, callback=callback_w(audio_input)):
-        frequency_analyser(high_bandwidth_output, low_bandwidth_output, audio_input)
+        # run function to perform frequency analysis on the queue.
+        frequency_analyser(high_bandwidth_output,
+                           low_bandwidth_output, audio_input)
 
 
 def frequency_analyser(high_bandwidth_output, low_bandwidth_output, audio_input):
@@ -35,18 +37,26 @@ def frequency_analyser(high_bandwidth_output, low_bandwidth_output, audio_input)
         low_bandwidth_output (Queue): A queue to place the downsampled fourier array
         audio_input (Queue): A queue of samples
     """
+    # the maximum number of samples to be included in the array at any given time
     max_size = ANALYSIS_SIZE * 40
+    # keeps a pointer of where the latest data is
     current_size = 0
+    # stores the data to be analysed once removed from the queue.
     audio_block = np.full(shape=(2, max_size), fill_value=np.nan)
+    # pointer to the first piece of data to be used in the next analysis.
     current_analysed = 0
-    logspace = np.logspace(0, np.log10((ANALYSIS_SIZE / 2)), BLUETOOTH_SAMPLES, dtype=int)
+    # used for downsampling according to how it would be displayed on a graph.
+    logspace = np.logspace(0, np.log10(
+        (ANALYSIS_SIZE / 2)), BLUETOOTH_SAMPLES, dtype=int)
 
+    # fetch from queue.
     sample_chunk = audio_input.get()
+    # while there is data in the queue, keep getting data.
     while True:
         try:
-            # Checks if buffer already full
+            # Checks if buffer already full, then does rollback.
+            # gets rid of all used data.
             if current_size + sample_chunk.size > max_size:
-                print("rollback!")
                 audio_block[:, :] = np.nan
                 current_size = 0
                 current_analysed = 0
@@ -58,27 +68,39 @@ def frequency_analyser(high_bandwidth_output, low_bandwidth_output, audio_input)
         except Empty:
             # frequency analysis
             working = current_analysed
+            # default data to correct size to prevent errors.
             magnitude = np.full(
                 shape=(2, int(ANALYSIS_SIZE / 2) + 1), fill_value=np.nan)
+            # bool to check if data has actually been analysed.
             magnituded = False
             # analyse as many times as queue allows(is actually only once when on pc)
             while (current_size - working) >= ANALYSIS_SIZE:
+                # a subset of the data, enough to get good results from 20Hz
                 sample_set = audio_block[:,
-                                            working:working + ANALYSIS_SIZE]
+                                         working:working + ANALYSIS_SIZE]
+                # does a fast fourier analysis on the sample data
                 magnitude = np.abs(np.fft.rfft(sample_set)) * 0.1
                 magnituded = True
+                # put the data into the high resolution queue for further analysis
                 for out in high_bandwidth_output:
                     out.put_nowait(magnitude)
+                # update the pointer to where the analysis should start next.
                 working += int(ANALYSIS_SIZE / FREQUENCY_OVERLAP)
             # if something changed add the most recent to the bluetooth queue
             if magnituded:
+                # complicated line: this takes the analysed data, splits it into chunks based on the logspace,
+                # takes the max of each of the chunks and reformats it to be the right shape.
                 less_magnitude = np.transpose(list(map(lambda x: list(
                     map(lambda y: max(magnitude[y, x[0] - 1: x[1]]), [0, 1])), np.transpose(
                     np.array([logspace, np.append(logspace[1:], logspace[-1])])))))
+                # put the downsampled data in the bluetooth queue.
                 low_bandwidth_output.put(
                     ("frequency", less_magnitude.tolist()))
+            # update where analysis should be looking next.
             current_analysed = working
+            # block
             sample_chunk = audio_input.get()
+
 
 def callback_w(audio_input):
     '''Called whenever new information comes into the sound input.
--- freq_visualiser.py
+++ freq_visualiser.py
@@ -12,15 +12,15 @@ def freq_visualiser_proc(channel, freq_in, low_in, low):
         Parameters:
             freq_in(Queue): a queue holding arrays of frequencies.
             channel(int): wihch channel of the frequency array to look at.'''
-    print("visualiser online")
 
+    # update_eq is called by matplotlib to get new data for graphs
     # pylint: disable-next=unused-argument
     def update_eq(frame):
-
-        # print(f"plotdata: {len(plotdata)}\nlatest: {len(latest)}")
-        # print(plotdata)
+        # this is a loop to make sure that when connected to bluetooth, you actually get a new set of frequency levels
+        # instead of just hiss and feedback data.
         got_freq = False
         while not got_freq:
+            # collect frequency data from a queue, different for bluetooth data and high res data.
             while True:
                 try:
                     if low:
@@ -33,6 +33,7 @@ def freq_visualiser_proc(channel, freq_in, low_in, low):
                         got_freq = True
                 except Empty:
                     break
+            # one more data get just to make sure it has a blocking wait at some point.
             if low:
                 new_data = low_in.get()
                 if new_data[0] == "frequency":
@@ -41,25 +42,29 @@ def freq_visualiser_proc(channel, freq_in, low_in, low):
             else:
                 data = freq_in.get()
                 got_freq = True
-        # print(f"data length {len(data[-1])}")
+        # give the graph the new data
         line[0].set_ydata(data[channel])
-        # print("updating")
         return line
 
+    # main code, sets up the graph.
+    # default data to prevent errors when no data is read in.
     data = np.zeros((2, BLUETOOTH_SAMPLES))
+    # get data from queue, make sure it is the data you want if reading from bluetooth queue.
     if low:
         new_data = low_in.get()
         if new_data[0] == "frequency":
             data = new_data[1]
     else:
         data = freq_in.get()
-    # print("got")
+    # set up the figure in matplotlib to display later.
     length = int(len(data[channel]))
-    # print(length)
     # pylint: disable-next=invalid-name
     fig, ax = plt.subplots()
     line = ax.plot(np.zeros(length))
 
+    # these are the scales, designed to match the downsampling in freq_analyser if coming from bluetooth,
+    # just to make sure the x axis scale is correct for the data.
+    # this complicated function is not needed on phone where it just displays linearly.
     if low:
         x_data = np.logspace(0, np.log10((math.ceil(SAMPLE_RATE/LOWEST_FREQUENCY))),
                              BLUETOOTH_SAMPLES, dtype=int)
@@ -68,13 +73,18 @@ def freq_visualiser_proc(channel, freq_in, low_in, low):
     else:
         x_data = np.linspace(num=length, start=0, stop=22050)
 
+    # graph is log scale to show the information as is common in the industry
     ax.set_xscale('log')
     line[0].set_xdata(x_data)
+    # sets defualt range to show as much data as needed.
     ax.axis((1, 22050, -5, 150))
 
+    # some config things.
     ax.tick_params(bottom=False, top=False, labelbottom=False,
                    right=False, left=False, labelleft=False)
     fig.tight_layout(pad=0)
+    # set up the animation to update the graph with new data every 3ms.
     # pylint: disable-next=unused-variable
     ani = FuncAnimation(fig, update_eq, interval=3, blit=True)
+    # display the graph. this blocks so need one process per graph.
     plt.show()
